4e2ee4f013e8dfeb96306f5c87c163e69eaca48e
msatoh <msatoh@hotmail.co.jp>
2022-01-05 16:07:37 +0900

    runnable on actual machine

--- seri.py
+++ seri.py
@@ -4,7 +4,43 @@ import time
 
 TIME_OUT=1
 
-def send( buf ):
+class dataset:
+    dataid=[0x00]
+    item=""
+    unit=""
+    nowdata=0x00
+    MAX=0x00
+    min=0xff
+    last_updated=0
+        
+    def __init__(self, nm):
+        self.item=nm
+        if nm=="coolant temp":
+            self.dataid=[0x00, 0x00, 0x08]
+            self.unit="℃"
+        elif nm=="inmani pressure":
+            self.dataid=[0x00, 0x00, 0x0d]
+            self.unit="kPa"
+        elif nm=="throttle sensor":
+            self.dataid=[0x00, 0x00, 0x15]
+            self.unit="%"
+    def calc_data(self):
+        if self.dataid==[0x00, 0x00, 0x08]:
+            return self.nowdata-40
+        elif self.dataid==[0x00, 0x00, 0x0d]:
+            return self.nowdata
+        elif self.dataid==[0x00, 0x00, 0x15]:
+            return self.nowdata*100/255
+    def input_data(self,data):
+        self.nowdata=data
+        self.last_updated=time.time()
+        if self.nowdata>self.MAX:
+            self.MAX=self.nowdata
+        if self.nowdata<self.min:
+            self.min=self.nowdata
+
+
+def send_msg( buf ):
     sent=False
     begin=time.time()
     while ser.out_waiting>=0:
@@ -23,7 +59,12 @@ def send( buf ):
         else:
             break
 
-def receive():
+def send_data(data):
+    length=len(data)
+    cs=[(128+16+240+length+sum(data))%256]
+    send_msg([0x80, 0x10, 0xf0]+[length]+data+cs)
+
+def receive_msg():
     mes_list=[]
     begin=time.time()
     while time.time()-begin < TIME_OUT or len(mes_list)==0:
@@ -35,14 +76,11 @@ def receive():
                 begin=time.time()
             else:
                 return "format error."
-#        elif len(mes_list)==3:
-#            if not(mes_list[1]==240 and mes_list[2]==16):
-#                mes_list=[]
         elif len(mes_list)==4:
             mes_len=mes_list[3]
         elif len(mes_list)>4:
             if mes_len==0:
-                if mes_list[len(mes_list)-1]==sum(mes_list[0:len(mes_list)-1])%256:
+                if mes_list[-1]==sum(mes_list[0:-1])%256:
                     if (mes_list[1]==240 and mes_list[2]==16):
                         return mes_list
                     else:
@@ -51,6 +89,27 @@ def receive():
                     return "check sum error."
             mes_len-=1
     return "timed out."
+
+def receive_data(sid,data):
+    message=receive_msg()
+    if not(message in [
+        "format error.",
+        "check sum error.",
+        "timed out."
+        ]):
+        if message[4]==0xe8:
+            datalength=len(data)
+            if message[3]==datalength+1:
+                for i in range(datalength):
+                    data[i].input_data(message[i+5])
+                    print(data[i].item,"=",data[i].calc_data(),data[i].unit)
+                print(" time:",data[i].last_updated,end="ms ""\n")
+            else:
+                print("number of data mismatched.")
+        else:
+            print("inapropriate responce.")
+    else:
+        print(message)
 
 ser = serial.Serial(
     port = "/dev/ttyUSB0",
@@ -63,9 +122,19 @@ ser = serial.Serial(
     #rtscts = 0,
     )
 
-if __name__=="__main__":
-    mes = [0x80, 0x10, 0xF0, 0x01, 0xBF, 0x40] #msg formatter needed.
-    while True:
-        send(mes)
-        print("rx: ",receive())
+def comm(sid, data):
+    if sid==0xa8:
+        comp_data=[0x00]
+        for i in range(len(data)):
+            comp_data=comp_data+data[i].dataid
+        send_data([sid]+comp_data)
+        receive_data(sid,data)
+    else:
+        send_data(data)
+        print(receive_msg())
 
+if __name__=="__main__":
+    data1=dataset("coolant temp")
+    data2=dataset("throttle sensor")
+    while True:
+        comm(0xa8, [data1,data2])
